48144418f56d545540dee10595f30d8168217e01
Dylan Emerson <emersonds2868@gmail.com>
2025-11-16 11:25:01 -0500

    Created package installer script

---
+++ scripts/package-installer.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Installing git.."
+sudo pacman -S git
+
+echo "Installing yay package manager..."
+cd $HOME
+DIRECTORY="$HOME/yay"
+
+if [ -d "$DIRECTORY" ]; then
+	echo  "yay already installed, proceeding..."
+else
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+fi
+
+echo "Installing terminal packages..."
+yay -S kitty lsd fastfetch zsh zsh-autosuggestions zsh-syntax-highlighting
+
+echo "Installing walker and elephant with basic provider lists..."
+yay -S walker elephant elephant-providerlist elephant-desktopapplications elephant-archlinuxpkgs elephant-files
+
+read -n1 -p "Would you like to install the additional elephant provider lists? (Y/n)" input
+case $input in
+	y|Y) yay -S elephant-calc elephant-clipboard elephant-menus elephant-runner elephant-symbols elephant-todo elephant-unicode elephant-websearch ;;
+	n|N) echo "\nSkipping additional elephant providerlists..." ;;
+	*) echo "\nInvalid input, skipping additional providerlists..." ;;
+esac
